cd5d0448bd4b863716c24c29378286c216a0032c
username-is-required <136007111+username-is-required@users.noreply.github.com>
2023-06-18 14:47:01 +0100

    started the function

--- packages/subinfo-update-checker/john-oliver/john-oliver.js
+++ packages/subinfo-update-checker/john-oliver/john-oliver.js
@@ -1,5 +1,53 @@
-async function main() {
+const { Octokit } = require("@octokit/core");
+const request = require("./requests.js");
 
+// helper function to wait for some time
+function wait(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+async function fetchValidJsonData(url) {
+    let data = await request.httpsGet(url);
+    
+    try {
+        data = JSON.parse(data);
+    } catch (err) {
+        console.log("Request to Reddit errored (bad JSON) [will retry]");
+        
+        // now we wait for 5 seconds and try it again
+        await wait(5000);
+        data = await fetchValidJsonData(url);
+    }
+
+    return data;
+}
+
+async function getParticipatingSubsList() {
+    let subs = [];
+    
+    let data = fetchValidJsonData("/r/ModCoord/wiki/index.json");
+    let text = data['data']['content_md'];
+    let lines = text.split("\n");
+    
+    for (let line of lines) {
+        if (line.startsWith("r/")) {
+            let subName = line.trim();
+            if (subName.slice(-1) == "/") subName = subName.slice(0, -1);
+            // exclude a single nonexistent sub that seems to be on the list for some reason
+            if (subName != "r/speziscool") subs.push(subName);
+        }
+    }
+
+    return subs;
+}
+
+async function main() {
+    console.log("** Started function **");
+
+    console.log("Getting list of participating subs");
+    let subs = getParticipatingSubsList();
+
+    
 }
 
 exports.main = main;
