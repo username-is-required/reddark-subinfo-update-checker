78254eaafa255c416a114c72f58bf81d50aaf584
username-is-required <136007111+username-is-required@users.noreply.github.com>
2023-06-19 11:03:17 +0100

    migrating to gcp (#2)
    * changed dependencies
    
    * Update index.js
    
    * started recoding for firestore
    
    * Update index.js
    
    * a few more changes
    
    * Update index.js
    
    * oops
    
    * Update index.js
    
    * Update index.js
    
    * Update index.js
    
    * Update README.md
    
    * Create job.yaml
    
    * Delete job.yaml

--- index.js
+++ index.js
@@ -2,13 +2,7 @@ const fs = require("fs");
 const fsPromises = fs.promises;
 const path = require("path");
 const { Octokit } = require("@octokit/core");
-const {
-    S3Client,
-    GetObjectCommand,
-    PutObjectCommand,
-    HeadObjectCommand,
-    NotFound
-} = require("@aws-sdk/client-s3");
+const { Firestore } = require("@google-cloud/firestore");
 const request = require("./requests.js");
 
 
@@ -16,20 +10,13 @@ const octokit = new Octokit({
     auth: process.env.GITHUB_ACCESS_TOKEN
 });
 
-const s3 = new S3Client({
-    forcePathStyle: false,
-    endpoint: "https://fra1.digitaloceanspaces.com",
-    credentials: {
-        accessKeyId: process.env.SPACES_KEY,
-        secretAccessKey: process.env.SPACES_SECRET
-    }
-});
+const firestore = new Firestore();
 
-
-const CLOUD_OBJECT_NAMES = {
-    STICKIED_POSTS_NUMBER: "stickied-posts-number.txt",
-    STICKIED_1: "stickied-1.txt",
-    STICKIED_2: "stickied-2.txt"
+const FIRESTORE_COLLECTION = "subinfo-update-checker";
+const FIRESTORE_FIELDS = {
+    STICKIED_POSTS_NUMBER: "stickied-posts",
+    STICKIED_1: "stickied-1",
+    STICKIED_2: "stickied-2"
 };
 
 
@@ -52,70 +39,19 @@ async function getFileContents(path) {
     }
 }
 
-// helper function to convert a stream to a string
-function streamToString(stream) {
-    let chunks = [];
-    
-    return new Promise((resolve, reject) => {
-        stream.on("data", chunk => chunks.push(Buffer.from(chunk)));
-        stream.on("error", err => reject(err));
-        stream.on("end", () => resolve(Buffer.concat(chunks).toString()));
-    });
-}
-
-// helper function to get the contents of a file stored in the cloud
-async function getCloudFileContents(path) {
-    let params = {
-        Bucket: process.env.SPACES_BUCKET,
-        Key: path
-    };
-
-    let command = new GetObjectCommand(params);
-    
-    try {
-        let response = await s3.send(command);
-        let contents = await streamToString(response.Body);
-
-        return contents;
-    } catch (err) {
-        console.log("Error occurred when reading file from Spaces - " + path + ": " + err);
-        console.log("Exiting process");
-        process.exit(1);
-    }
-}
-
-// helper function to save a file with given name and contents to the cloud
-async function saveCloudFile(path, contents) {
-    let params = {
-        Bucket: process.env.SPACES_BUCKET,
-        Key: path,
-        Body: contents
-    };
-
-    let command = new PutObjectCommand(params);
-
-    try {
-        await s3.send(command);
-    } catch (err) {
-        console.log("Error occurred when writing file to Spaces - " + path + ": " + err);
-        console.log("Exiting process");
-        process.exit(1);
-    }
-}
-
 async function fetchValidJsonData(url) {
     let data = await request.httpsGet(url);
     
     try {
         data = JSON.parse(data);
     } catch (err) {
-        console.log(url + ": Request to Reddit errored (bad JSON) [will retry]");
+        console.log(url + ": Request errored (bad JSON) [will retry] - " + url);
         
         // now we wait for 5 seconds and try it again
         await wait(5000);
         data = await fetchValidJsonData(url);
     }
-
+    
     return data;
 }
 
@@ -160,47 +96,26 @@ async function getSubData(subName) {
     return subData;
 }
 
-async function subHasStoredStickiedPosts(subName) {
-    let path = subName + "/" + CLOUD_OBJECT_NAMES.STICKIED_POSTS_NUMBER;
+function getSubFirestoreDocRef(subName) {
+    return firestore.collection(FIRESTORE_COLLECTION)
+        .doc(subName.substring(2));
+}
+
+async function getPrevStickiedPosts(subName) {
+    let doc = getSubFirestoreDocRef(subName);
+    doc = await doc.get();
+
+    if (!doc.exists) return null;
+
+    doc = doc.data();
     
-    let params = {
-        Bucket: process.env.SPACES_BUCKET,
-        Key: path
-    }
-
-    let command = new HeadObjectCommand(params);
-    
-    try {
-        await s3.send(command);
-        return true;
-    } catch (err) {
-        if (err instanceof NotFound) return false;
-        
-        console.log("Error occurred when checking if file exists in Spaces - " + path + ": " + err);
-        console.log("Exiting process");
-        process.exit(1);
-    }
-}
-
-async function getPrevNumberOfStickiedPosts(subName) {
-    let data = await getCloudFileContents(subName + "/" + CLOUD_OBJECT_NAMES.STICKIED_POSTS_NUMBER);
-    return parseInt(data);
-}
-
-async function setNumberOfStickiedPosts(subName, numOfStickiedPosts) {
-    await saveCloudFile(
-        subName + "/" + CLOUD_OBJECT_NAMES.STICKIED_POSTS_NUMBER,
-        numOfStickiedPosts.toString()
-    );
-}
-
-async function getPrevStickiedPostsText(subName) {
     let prevStickiedPostsText = [];
 
-    let prevNumOfStickiedPosts = await getPrevNumberOfStickiedPosts(subName);
+    let prevNumOfStickiedPosts =
+        doc[FIRESTORE_FIELDS.STICKIED_POSTS_NUMBER];
     
     for (let i = 0; i < prevNumOfStickiedPosts; i++) {
-        let postText = await getCloudFileContents(subName + "/" + CLOUD_OBJECT_NAMES["STICKIED_" + (i+1)]);
+        let postText = doc[FIRESTORE_FIELDS["STICKIED_" + (i+1)]];
         prevStickiedPostsText.push(postText);
     }
 
@@ -209,15 +124,17 @@ async function getPrevStickiedPostsText(subName) {
 
 async function saveStickiedPosts(subName, stickiedPosts) {
     if (stickiedPosts.length > 2) throw new Error("cannot be more than 2 stickied posts to save");
+
+    let doc = getSubFirestoreDocRef(subName);
+
+    let data = {};
+
+    data[FIRESTORE_FIELDS.STICKIED_POSTS_NUMBER] = stickiedPosts.length;
     
-    await setNumberOfStickiedPosts(stickiedPosts.length);
-    
-    for (let i in stickiedPosts) {
-        await saveCloudFile(
-            subName + "/" + CLOUD_OBJECT_NAMES["STICKIED_" + (i+1)],
-            stickiedPosts[i].selftext
-        );
-    }
+    for (let i in stickiedPosts)
+        data[FIRESTORE_FIELDS["STICKIED_" + (i+1)]] = stickiedPosts[i].selftext;
+
+    await doc.set(data, { merge: false });
 }
 
 async function createGithubIssue(title, body) {
@@ -296,24 +213,21 @@ async function main() {
             
             console.log(subName + ": already johnolivered. checking if review required");
 
-            let subHasStoredStickiedPosts = await subHasStoredStickiedPosts(subName);
-            if (subHasStoredStickiedPosts) {
-                let prevStickiedPosts = await getPrevStickiedPosts(subName);
-                if (stickiedPosts.length == prevStickiedPosts.length) {
-                    let allStickiedPostsMatch = true;
-                    
-                    for (let i in stickiedPosts) {
-                        if (stickiedPosts[i].selftext != prevStickiedPosts[i]) {
-                            allStickiedPostsMatch = false;
-                            break;
-                        }
+            let prevStickiedPosts = await getPrevStickiedPosts(subName);
+            if (prevStickiedPosts != null && stickiedPosts.length == prevStickiedPosts.length) {
+                let allStickiedPostsMatch = true;
+                
+                for (let i in stickiedPosts) {
+                    if (stickiedPosts[i].selftext != prevStickiedPosts[i]) {
+                        allStickiedPostsMatch = false;
+                        break;
                     }
-                    
-                    if (allStickiedPostsMatch) {
-                        // all checks have passed - this sub doesn't need review
-                        console.log(subName + ": checks passed, no review required");
-                        continue;
-                    }
+                }
+                
+                if (allStickiedPostsMatch) {
+                    // all checks have passed - this sub doesn't need review
+                    console.log(subName + ": checks passed, no review required");
+                    continue;
                 }
             }
 
@@ -339,24 +253,21 @@ async function main() {
             if (containsJohnOliver) {
                 console.log(subName + ": matches john oliver filter. checking if review required");
                 
-                let subHasStoredStickiedPosts = await subHasStoredStickiedPosts(subName);
-                if (subHasStoredStickiedPosts) {
-                    let prevStickiedPosts = await getPrevStickiedPosts(subName);
-                    if (stickiedPosts.length == prevStickiedPosts.length) {
-                        let allStickiedPostsMatch = true;
-                        
-                        for (let i in stickiedPosts) {
-                            if (stickiedPosts[i].selftext != prevStickiedPosts[i]) {
-                                allStickiedPostsMatch = false;
-                                break;
-                            }
+                let prevStickiedPosts = await getPrevStickiedPosts(subName);
+                if (prevStickiedPosts != null && stickiedPosts.length == prevStickiedPosts.length) {
+                    let allStickiedPostsMatch = true;
+                    
+                    for (let i in stickiedPosts) {
+                        if (stickiedPosts[i].selftext != prevStickiedPosts[i]) {
+                            allStickiedPostsMatch = false;
+                            break;
                         }
-                        
-                        if (allStickiedPostsMatch) {
-                            // all checks have passed - this sub doesn't need review
-                            console.log(subName + ": no change, no review required");
-                            continue;
-                        }
+                    }
+                    
+                    if (allStickiedPostsMatch) {
+                        // all checks have passed - this sub doesn't need review
+                        console.log(subName + ": no change, no review required");
+                        continue;
                     }
                 }
                 
@@ -378,4 +289,4 @@ async function main() {
     console.log("** Function complete **");
 }
 
-exports.main = main;
+main();
